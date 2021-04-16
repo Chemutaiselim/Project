f7ec57885f7156601c9a904acc226c39b1295b08
Selim <selimchemutai19@gmail.com>
2021-04-16 16:43:29 +0300

    Strings

---
+++ src/convert data types.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val a=3
+    val aDouble=a.toDouble()
+
+    println(aDouble)
+    /*
+    Kotlin can automatically set the data type
+
+     */
+    val longNum:Long=123456709876L
+    var no: String ="2"
+    print(longNum)
+
+
+}
