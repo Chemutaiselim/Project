e2ab221e30e06469d887613054371d9514d78e1e
Selim <selimchemutai19@gmail.com>
2021-04-16 15:27:17 +0300

    added converting data types

--- src/variables.kt
+++ src/variables.kt
@@ -1,9 +1,23 @@
 fun main(args: Array<String>) {
     var name="JonnyB"
+    /* types
+    string letters
+    boolean true/false
+    string int whole numbers
+    types of no, int double
+    double decimal holds bigger decimals 64bit
+    float  holds smaller decimals 32 bit to use float specify
+    e.g. float, val = 123.45f
+
+
+
+     */
+    name="batman"
     var isAwesome=true
+    isAwesome=false
     println("is"+name+"awesome?")
     println("the awnser is:"+isAwesome)
-
+//best to use val
     var a=8
     var b=7
     println(a+b)
