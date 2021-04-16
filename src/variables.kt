a2248dbb417475c4f9677b3e27c316194a11fb47
Selim <selimchemutai19@gmail.com>
2021-04-16 15:09:22 +0300

    variables, hello world

---
+++ src/variables.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var name="JonnyB"
+    var isAwesome=true
+    println("is"+name+"awesome?")
+    println("the awnser is:"+isAwesome)
+
+    var a=8
+    var b=7
+    println(a+b)
+}
