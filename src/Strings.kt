ab5c937ef75ad9379aa92f9ee777dbe3e4c8a8d9
Selim <selimchemutai19@gmail.com>
2021-04-18 10:05:18 +0300

    string

--- src/Strings.kt
+++ src/Strings.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
-
+val str= "may the force be with you"
+    println(str)
+    //\" quotation
+    //\t tab
+    //\n
+    //\b
+    //\r
+    //\\
+    //\$
+//raw string
+    val threequo=""">a long time ago
+        >in a galaxy 
+        |far,far, away...
+        |BUMM BUMM BUMM""".trimMargin(">")
+    println (threequo)
+   // for(char in str){
+       // println(char)
+       val contentequals= str.contentEquals("May the force be with you")
+    println(contentequals)
 }
