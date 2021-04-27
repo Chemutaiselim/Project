7c3f7d5446418d5e7b7c651431ccb0162f0c097d
Selim <selimchemutai19@gmail.com>
2021-04-27 12:36:02 +0300

    Java

---
+++ src/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void test(int x, double d, char c, int y[]){
+        /* method statements */
+    }
+    public static void main(String[] args) {
+        int [] val = {0, 1, 2};
+        test(3,2.1, 'c',val);
+        /* method call */
+        }
+    }
+
+
--- src/Excecute.java
+++ src/Excecute.java
@@ -1,2 +1,15 @@
 public class Excecute {
+    public static void changeArray(int[] data)
+    {
+        for (int k = data.length - 1; k > 0; k--)
+            data[k - 1] = data[k] + data[k - 1];
+    }
+    public static void main(String[] args) {
+        int[] d = {1, 6, 12};
+        changeArray(d);
+      for (int element:d){
+          System.out.println(element);
+      }
+    }
+
 }
