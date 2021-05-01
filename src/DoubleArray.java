46deb3e02bab21706b81b86a29a821df2b001c7d
Selim <selimchemutai19@gmail.com>
2021-05-01 15:45:20 +0300

    Java

--- src/DoubleArray.java
+++ src/DoubleArray.java
@@ -1,2 +1,7 @@
 public class DoubleArray {
+    public static void findValue(double[] a){
+        int i= (int) 98.22;
+           System.out.println(i);
+
+    }
 }
---
+++ src/Score.java
@@ -0,0 +1,13 @@
+
+public class Score{
+    public static int secret(int w) {
+        while (w%10!=2){
+            w+=4;
+        }
+        return w;
+    }
+    public static void main(){
+        int b=4;
+        System.out.print(secret(b-b));
+    }
+    }
